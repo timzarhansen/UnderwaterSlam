7b6a2ca261599f7b0f20493976a1ec83806848fd
timhansen <timhansen93@googlemail.com>
2025-02-18 17:29:17 +0100

    hopefully working update

--- src/slamTools/src/slamToolsRos.cpp
+++ src/slamTools/src/slamToolsRos.cpp
@@ -1311,8 +1311,8 @@ slamToolsRos::getDatasetFromGraphforPoseArray(std::vector<Eigen::Matrix4d>& data
     // double maxOverall = 0;
     for (int i = 0; i < graphSaved.getVertexList()->size(); i++)
     {
-        // Eigen::Matrix4d transformation = graphSaved.getVertexList()->at(i).getTransformation();
-        Eigen::Matrix4d transformation = graphSaved.getVertexList()->at(i).getGroundTruthTransformation();
+        Eigen::Matrix4d transformation = graphSaved.getVertexList()->at(i).getTransformation();
+        // Eigen::Matrix4d transformation = graphSaved.getVertexList()->at(i).getGroundTruthTransformation();
 
         dataSet.push_back(transformation);
     }
